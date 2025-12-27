34f828d52320d88bf4e7a731f9ee950765d2fa70
Rodrigo Lopes Velloso <135156904+velloso99@users.noreply.github.com>
2025-12-27 18:41:26 -0300

    dados

--- dados.py
+++ dados.py
@@ -5,3 +5,11 @@ import sqlite3 as lite
 con = lite.connect('dados.db')
 
 #Criando a tabela
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE Quantia(id INTEGER PRIMARY KEY AUTOINCREMENT, valor DECIMAL)")
+
+#tabela de despesas
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE Despesas(id INTEGER PRIMARY KEY AUTOINCREMENT, categoria TEXT, descricao TEXT, valor DECIMAL)")
